55ac8044e7dff29f28752aba619c3a9c55634ac9
Nick Beattie <nick@nickbytes.com>
2018-08-05 17:48:45 -0400

    refactor: refresh posts and stateless components (#12)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,71 +1,55 @@
-import React from 'react';
-import ContentSelection from '../ContentSelection';
+import React from "react";
+import ContentSelection from "../ContentSelection";
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contentSelectionOpen: false
-    };
-  }
+const Header = ({
+  getPosts,
+  contentSelectionOpen,
+  postDisplay,
+  togglePostDisplay,
+  toggleContentSelection
+}) => (
+  <header className={`Header`}>
+    <div className={`Header__Nav`}>
+      <a
+        className={`${"ContentToggle"} ${
+          contentSelectionOpen ? "ContentToggle_Open" : ""
+        }`}
+        onClick={() => toggleContentSelection()}
+      >
+        <img src="/images/icon-plus.svg" alt="plus icon" />
+      </a>
+      <div className={"ContentDisplayToggle"}>
+        <a
+          className={`
+          ${"ContentDisplayToggle__Item"} 
+          ${postDisplay === "mine" ? "ContentDisplayToggle__Item_Selected" : ""}
+        `}
+          onClick={() => togglePostDisplay("mine")}
+        >
+          <img src="/images/icon-smile.svg" alt="show my posts" />
+        </a>
+        <a
+          className={`
+          ${"ContentDisplayToggle__Item"} 
+          ${
+            postDisplay === "theirs"
+              ? "ContentDisplayToggle__Item_Selected"
+              : ""
+          }
+        `}
+          onClick={() => togglePostDisplay("theirs")}
+        >
+          <img src="/images/icon-group.svg" alt="show their posts" />
+        </a>
+      </div>
+    </div>
 
-  componentDidMount() {}
-
-  toggleContentSelection = () => {
-    this.setState({
-      contentSelectionOpen: !this.state.contentSelectionOpen
-    });
-  };
-
-  render() {
-    return (
-      <header className={`Header`}>
-        <div className={`Header__Nav`}>
-          <a
-            className={`${'ContentToggle'} ${
-              this.state.contentSelectionOpen ? 'ContentToggle_Open' : ''
-            }`}
-            onClick={() => this.toggleContentSelection()}
-          >
-            <img src="/images/icon-plus.svg" alt="plus icon" />
-          </a>
-          <div className={'ContentDisplayToggle'}>
-            <a
-              className={`
-								${'ContentDisplayToggle__Item'} 
-								${
-                  this.props.postDisplay === 'mine'
-                    ? 'ContentDisplayToggle__Item_Selected'
-                    : ''
-                }
-							`}
-              onClick={() => this.props.togglePostDisplay('mine')}
-            >
-              <img src="/images/icon-smile.svg" alt="show my posts" />
-            </a>
-            <a
-              className={`
-								${'ContentDisplayToggle__Item'} 
-								${
-                  this.props.postDisplay === 'theirs'
-                    ? 'ContentDisplayToggle__Item_Selected'
-                    : ''
-                }
-							`}
-              onClick={() => this.props.togglePostDisplay('theirs')}
-            >
-              <img src="/images/icon-group.svg" alt="show their posts" />
-            </a>
-          </div>
-        </div>
-        <ContentSelection
-          getPosts={this.props.getPosts}
-          open={this.state.contentSelectionOpen}
-          toggleContentSelection={this.toggleContentSelection}
-        />
-      </header>
-    );
-  }
-}
+    <ContentSelection
+      getPosts={getPosts}
+      open={contentSelectionOpen}
+      toggleContentSelection={toggleContentSelection}
+    />
+  </header>
+);
 
 export default Header;
